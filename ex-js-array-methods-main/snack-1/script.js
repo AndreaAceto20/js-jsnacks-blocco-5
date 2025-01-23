2586d2a17afb6432d55f9c7696084187da457ab1
Andrea Aceto <andrea.aceto20@gmail.com>
2025-01-23 18:22:36 +0100

    [DONE] Snack1, Snack2

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+for (i = 0 ; i < names.length; i++){
+    console.log(names[i]);
+}
