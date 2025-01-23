c35541b3b9ece543846fde13582839eeec0a049a
Andrea Aceto <andrea.aceto20@gmail.com>
2025-01-23 18:51:39 +0100

    [DONE]Snack7, Snack8

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,12 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let id2 = students.filter((element) =>{
+  if(element.id === 2){
+    return true
+  }
+  return false
+})
+
+console.log(id2);
--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+let marco = students.find((element) =>{
+  return students.name = ("Marco Lanci")
+})
+console.log(marco.class);
